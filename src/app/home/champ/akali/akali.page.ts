ed627bdb976fd6b77d03e826b013a7c6b4af5bf7
Luiz Zanutto <48605865+louizlv@users.noreply.github.com>
2021-05-19 00:52:52 -0300

    Feature - Personagem favorito

--- src/app/home/champ/akali/akali.page.ts
+++ src/app/home/champ/akali/akali.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FavchampsService } from 'src/app/services/favchamps.service';
 
 @Component({
   selector: 'app-akali',
   templateUrl: './akali.page.html',
   styleUrls: ['./akali.page.scss'],
 })
-export class AkaliPage implements OnInit {
+export class AkaliPage {
 
-  constructor() { }
+  public champhere;
+  
+  constructor(private favchampsService: FavchampsService) {
+      this.champhere = 
+        {nome: 'AKALI', 
+        photo: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg', 
+        link: '/home/champ/akali'}; 
+  }
 
-  ngOnInit() {
+  public addFavorite() {
+    this.favchampsService.favoritados.length = 0;
+    this.favchampsService.favoritados.push(this.champhere);
+    this.favchampsService.update();
+    console.log(this.favchampsService.favoritados)
   }
 
 }
