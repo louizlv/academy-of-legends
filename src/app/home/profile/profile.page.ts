8987e6511b02a89ce38af930baa3de87e33e1702
leoneves123321 <83130202+leoneves123321@users.noreply.github.com>
2021-06-06 20:56:25 -0300

    att_summoner

--- src/app/home/profile/profile.page.ts
+++ src/app/home/profile/profile.page.ts
@@ -1,13 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { apiInvocador } from 'src/app/modulos/apiInvocador.module';
+
+export interface Invocador{
+  id : string;
+  acount_id : string;
+  nome : string;
+  profileIconId: number;
+  summonerLevel : string;
+  revisionDate : number;
+}
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage  {
+  
+  public summoner;
+  public invocadorId: string;
+  public invocadorNome: string;
+  public id_conta: string;
+  public nivel_invocador: number;
+  
+  public currentSummoner: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private invocador: apiInvocador) {
+    this.currentSummoner = route.snapshot.paramMap.get('id');
+}
+
+public async saveInfo(){
+
+  this.invocadorId = this.invocador.data[this.currentSummoner].id;
+  this.id_conta = this.invocador.data[this.currentSummoner].acount_id;
+  this.invocadorNome = this.invocador.data[this.currentSummoner].invocadorNome;
+  this.nivel_invocador = this.invocador.data[this.currentSummoner].summonerLevel;
+  this.summoner= {
+   nome: this.invocadorNome,
+   id: this.id_conta,
+   link: 'home/inicio/' + this.invocadorId
+  };
+}
 
   ngOnInit() {
   }
--- src/app/home/profile/summoner/summoner.page.ts
+++ src/app/home/profile/summoner/summoner.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { apiInvocador } from 'src/app/modulos/apiInvocador.module';
 
 @Component({
   selector: 'app-summoner',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SummonerPage implements OnInit {
 
-  constructor() { }
+  public summoner;
+  public invocadorId: string;
+  public invocadorNome: string;
+  public id_conta: string;
+  public nivel_invocador: number;
+  
+  public currentSummoner: string;
+
+  constructor(private route: ActivatedRoute, private invocador: apiInvocador) {
+    this.currentSummoner = route.snapshot.paramMap.get('id');
+}
+
+public async saveInfo(){
+
+  this.invocadorId = this.invocador.data[this.currentSummoner].id;
+  this.id_conta = this.invocador.data[this.currentSummoner].acount_id;
+  this.invocadorNome = this.invocador.data[this.currentSummoner].invocadorNome;
+  this.nivel_invocador = this.invocador.data[this.currentSummoner].summonerLevel;
+  this.summoner= {
+   nome: this.invocadorNome,
+   id: this.id_conta,
+   link: 'home/inicio/' + this.invocadorId
+  };
+}
 
   ngOnInit() {
   }
